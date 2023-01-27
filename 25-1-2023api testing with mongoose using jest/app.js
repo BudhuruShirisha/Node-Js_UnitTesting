809d6088b83da5fbc427c2d7a7a37f309c9cbac0
BudhuruShirisha <budhurushirisha7@gmail.com>
2023-01-27 17:05:17 +0530

    async fucntion test cases with json

--- 25-1-2023api testing with mongoose using jest/app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const app = express()
-const studentRoute = require("./routes/student_route")
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/student", studentRoute)
-module.exports = app
--- 25-1-2023api testing with mongoose using jest/index.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app")
-
-app.listen(2900, () => {
-    console.log("app lisening at the 2900")
-})
